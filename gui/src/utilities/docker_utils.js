26e65f785b93b0e7ebb34a5de8550444cd2700d3
AdrianaCannata <adrianacannata2000@gmail.com>
2025-07-11 21:56:52 +0200

    repository polished - removed unused file

--- gui/src/utilities/docker_utils.js
+++ gui/src/utilities/docker_utils.js
@@ -1,119 +1,49 @@
-import fs from 'fs';
-import os from 'os';
-const yaml = require('js-yaml');
-const path = require ("path");
-const Docker = require("dockerode");
+// utils to check Docker installation
 
-let docker;
-const platform = os.platform();
+import { exec, execSync } from 'child_process';
+import os                 from 'os';
+import Docker             from 'dockerode';
 
-if (platform === "win32") {
-    docker = new Docker({ socketPath: '//./pipe/docker_engine' });
-} else if (platform === "linux" || process.platform === "darwin") {
-    docker = new Docker({socketPath: '/var/run/docker.sock'});
-} else {
-    console.error("Unsupported platform: ", process.platform);
-}
-  
-docker.ping((err, data) => {
-if (err) {
-    console.error("Docker connection failed: ", err);
-} else {
-    console.log("Docker is connected: ", data);
-}
-});
-
-export async function prepareSnakemakeCommand(userInput, containerName = "amazing_blackburn", userConfigPath) {
-    const container = docker.getContainer(containerName);
-    try {
-        // await restartIfNeeded(container, containerName);
-        const newContainer = await mapIO(containerName, userInput, userConfigPath);
-
-        // inspect container
-        console.log("Inspecting container...");
-        const data = await newContainer.inspect();
-        const volumes = data.Mounts;
-            
-        // Se non ci sono volumi, segnaliamo che non ci sono
-        if (!volumes || volumes.length === 0) {
-            console.log('No volumes mounted on this container.');
-            return;
-        }
-
-        volumes.forEach(volume => {
-            console.log(`Source: ${volume.Source}, Target: ${volume.Target}, Type: ${volume.Type}`);
-        });
-        return newContainer;    
-    } catch(error) {
-        console.error("Error in preparing execution: ", error);
-        throw(error);
+async function connectToDocker () {
+  return new Promise((resolve, reject) => {
+    const platform = os.platform();
+    let docker;
+    if (platform === 'win32') {
+      docker = new Docker({ socketPath: '//./pipe/docker_engine' });
+    } else if (platform === 'linux' || process.platform === 'darwin') {
+      docker = new Docker({ socketPath: '/var/run/docker.sock' });
+    } else {
+      reject('Unsupported platform: ', process.platform);
     }
-    
+    docker.ping((err, data) => {
+      if (err) {
+        reject('Docker connection failed: ', err);
+      } else {
+        resolve();
+      }
+    });
+  });
 }
 
+export async function checkDockerInstalled () {
+  try {
+    await connectToDocker();
+    // execSync("docker --version", { stdio: "ignore" });
+    const response = 'Docker is installed';
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log('Docker is not installed.');
+    console.error(error);
+    const platform = os.platform();
 
-async function mapIO(userInput, userConfigPath) {
-    const userOutput = path.join(userInput, "output");
-    const containerInput = "/project/user-input";
-    const containerOutput = path.join(containerInput, "output");
-    const containerConfigPath = "/project/snakemake";
-    // non serve più, si monta direttamente userData
-    // const snakemakeDirectory = path.dirname(userConfigPath);
-    // const userResources = path.join(snakemakeDirectory, "/resources");
-    // const snakeFile = path.join(snakemakeDirectory, "/Snakefile");
-
-    const containerName = "zealous_ishizaka"
-    const container = docker.getContainer(containerName);
-    
-    if (!container) {
-        throw new Error(`Container ${containerName} not found`);
-    }
-
-    console.log(`Working on: (name) ${containerName} container ${container}`);
-
-    try {
-        // Fermare il container se è in esecuzione
-        const containerInfo = await container.inspect();
-        if (containerInfo.State.Status === 'running') {
-            console.log(`Stopping container ${containerName}...`);
-            await container.stop();
-            console.log(`Container ${containerName} stopped.`);
-        }
-
-        // Eliminare il container esistente
-        console.log(`Removing container ${containerName}...`);
-        await container.remove();
-        console.log(`Container ${containerName} removed.`);
-
-        // Ricreare il container con i nuovi volumi
-        console.log(`Recreating container ${containerName} with updated volumes...`);
-        try {
-            await docker.createContainer({
-                Image: containerInfo.Config.Image, // Usa l'immagine del container originale
-                name: containerName,
-                Cmd: ["/bin/bash", "-c", `mkdir -p ${containerInput} && mkdir -p ${containerOutput} && mkdir -p ${containerConfigPath} $$ \
-                    while true; do sleep 30; done`],
-                HostConfig: {
-                    Binds: [
-                        `${userInput}:${containerInput}`,
-                        `${userOutput}:${containerOutput}`,
-                        `${userConfigPath}:${containerConfigPath}`, //in userConfigPath passo userData/snakemake
-                    ]
-                }
-            });
-            
-        } catch(error) {
-            throw(error);
-        }
-        // Avviare il nuovo container
-        const newContainer = docker.getContainer(containerName);
-        await newContainer.start();
-
-        console.log('Container recreated and started with updated volumes.');
-        return newContainer;
-
-    } catch (error) {
-        console.error("Error while dynamic binding of volumes: ", error);
-    }
+    if (platform === 'linux' || platform === 'win32' || platform === 'darwin')
+      return `Docker not found: please install. Error: ${error}`;
+    else
+      return 'System not supported for Docker installation';
+  }
 }
 
+export async function checkDockerRunning () {
+  await connectToDocker();
+}
